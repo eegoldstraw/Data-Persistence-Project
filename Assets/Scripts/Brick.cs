66a17ff310ef257141f5342030a32b9cba7d553f
eegoldstraw <eegoldstraw@gmail.com>
2022-09-23 10:57:51 +0100

    Change brick colours
    Change brick colours to blue, cyan and magenta.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -18,16 +18,16 @@ public class Brick : MonoBehaviour
         switch (PointValue)
         {
             case 1 :
-                block.SetColor("_BaseColor", Color.green);
+                block.SetColor("_BaseColor", Color.cyan);
                 break;
             case 2:
-                block.SetColor("_BaseColor", Color.yellow);
+                block.SetColor("_BaseColor", Color.magenta);
                 break;
             case 5:
                 block.SetColor("_BaseColor", Color.blue);
                 break;
             default:
-                block.SetColor("_BaseColor", Color.red);
+                block.SetColor("_BaseColor", Color.yellow);
                 break;
         }
         renderer.SetPropertyBlock(block);
